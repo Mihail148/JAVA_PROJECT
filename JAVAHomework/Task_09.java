3a1a8b907d7a52c983724b700462707c92ce7f28
Mihail Rkitcky <papi.pay@mail.ru>
2023-04-26 10:02:04 +0300

    Задача № 9, 10

---
+++ JAVAHomework/Task_09.java
@@ -0,0 +1,23 @@
+//1. Реализовать консольное приложение, которое:
+//Принимает от пользователя и “запоминает” строки.
+//Если введено print, выводит строки так, чтобы последняя введенная была первой в списке, а первая - последней.
+//Если введено revert, удаляет предыдущую введенную строку из памяти.
+
+package JAVAHomework;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Task_09 {
+    public static void main(String[] args) {
+        LinkedList<String> inputLinkedList = new LinkedList<>();
+        Scanner scanner = new Scanner(System.in);
+        String srtFromCons;
+        System.out.println("Введите значение для строки.\nДля выхода используйте 'print'");
+
+        while (!(srtFromCons = scanner.nextLine()).equals("print"))
+            inputLinkedList.addFirst(srtFromCons);
+        scanner.close();
+        System.out.println("Получился перевёрнутый LinkedList: " + inputLinkedList.toString());
+}
+}
